44ebe4d6785d8f2d5a8355876e4a943b2cd9b5d8
Zor Rothman <zor.rothman@gmail.com>
2019-11-25 20:10:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,58 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+#
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board
+
+
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+puts display_board(board)
+#
+# def display_board(board)
+#   puts " #{board[0]} |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+# end
+#
+# puts display_board(board)
+#
+# def display_board(board)
+#   puts " #{board[0]} |   |   "
+#   puts "-----------"
+#   puts "   | #{board[4]} |   "
+#   puts "-----------"
+#   puts "   |   |   "
+# end
+#
+# puts display_board(board)
+
+
+# def display_board(board)
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+#   puts "-----------"
+#   puts "   |   |   "
+# end
+#
+#
+# # puts "prints a board with an X in the center position #{display_board(board)}"
+# puts display_board(board)
